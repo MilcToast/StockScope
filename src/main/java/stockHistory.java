72a15d057e69589e5fda11527761ff99687d22fa
jem.h <jeremyhhj@gmail.com>
2025-06-15 14:49:16 -0400

    updated to protect sensitivity (apikey)

--- src/main/java/stockHistory.java
+++ src/main/java/stockHistory.java
@@ -1,25 +1,35 @@
-import java.net.URL;
-import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import com.google.gson.*;
 
 public class stockHistory {
     public static void main(String[] args) {
 
-        String apiKey = "C9RGET7L238ZT2QE";
+        String apiKey = Secret.apiKey;
         String symbol = "GOOG";
 
-        String urlString ="https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY"
+        String url ="https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY"
                 + "&symbol=" + symbol
                 + "&apikey=" + apiKey;
 
 
         try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            // Create HTTP client
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            // Create HTTP request
+            HttpRequest getRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
             Gson gson = new Gson();
 
-            int responseCode = connection.getResponseCode();
+            HttpResponse<String> response = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            gson.fromJson(response.body(), Data.class);
 
 
         } catch (Exception e) {
